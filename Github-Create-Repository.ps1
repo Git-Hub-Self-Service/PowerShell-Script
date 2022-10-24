cf749280b83128b3c3adbf05db95b9eee4e278a3
dtodescx <105944588+dtodescx@users.noreply.github.com>
2022-10-24 01:35:27 -0300

    Add files via upload

---
+++ Github-Create-Repository.ps1
@@ -0,0 +1,32 @@
+PARAM
+(
+	[Parameter(Mandatory = $False, Position = 0)]
+    [String] $RepositoryName='CreateFromActions',
+    [String] $Org='Git-Hub-Self-Service'
+ )
+
+# authentication header
+
+$username = "Username";
+$token = "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";
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$token)));
+$authHeader = @{"Authorization"="Basic $base64AuthInfo"};
+
+# create repository
+$body = @{
+	name = $RepositoryName;
+	auto_init = $true;
+} | ConvertTo-Json -Compress;
+
+if($Org) 
+{
+	$creationUri = 'https://api.github.com/orgs/{0}/repos' -f $Org;
+}
+else 
+{
+	$creationUri = 'https://api.github.com/user/repos';
+}
+$repoCreationResult = Invoke-RestMethod -Uri $creationUri -Headers $authHeader -Method Post -Body $body;
+Write-Host ("Repository '{0}' created" -f $RepositoryName) -foregroundcolor "green";
+
+Start-Sleep -Seconds 3;
