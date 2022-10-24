2a180ff61da1ec4a582e469986bf0400cdb66e2f
dtodescx <105944588+dtodescx@users.noreply.github.com>
2022-10-24 02:23:13 -0300

    Update Github-Create-Repository.ps1

--- Github-Create-Repository.ps1
+++ Github-Create-Repository.ps1
@@ -26,7 +26,5 @@ else
 {
 	$creationUri = 'https://api.github.com/user/repos';
 }
-#$repoCreationResult = Invoke-RestMethod -Uri $creationUri -Headers $authHeader -Method Post -Body $body;
-Write-Host ("Repository '{0}' created" -f $RepositoryName) -foregroundcolor "green";
 
 Start-Sleep -Seconds 3;
