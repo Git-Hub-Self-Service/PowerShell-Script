bddd2dc3fb3325ab8cd7b80347d510be93812a70
dtodescx <105944588+dtodescx@users.noreply.github.com>
2022-10-25 01:15:42 -0300

    Update Installmodule.ps1

--- Installmodule.ps1
+++ Installmodule.ps1
@@ -1,35 +1,3 @@
 # Install-Module -Name ImportExcel -force
-Function Read-Excel   {
 
-
-#$Excelfile = "https://intel-my.sharepoint.com/:x:/r/personal/diomendesx_todesco_intel_com/_layouts/15/Doc.aspx?sourcedoc=%7BBE481FE1-E9D8-463C-8353-C0C430011FA6%7D&file=RepoGitHUb.xlsx&action=default&mobileredirect=true&clickparams=eyJBcHBOYW1lIjoiVGVhbXMtRGVza3RvcCIsIkFwcFZlcnNpb24iOiIyNy8yMjA5MDQwMDcxMiIsIkhhc0ZlZGVyYXRlZFVzZXIiOmZhbHNlfQ%3D%3D&cid=c2281e22-e63a-44ac-a46c-b4be14b5ba09"
-$Excelfile = "https://intel-my.sharepoint.com/:x:/r/personal/diomendesx_todesco_intel_com/_layouts/15/Doc.aspx?sourcedoc=%7BBE481FE1-E9D8-463C-8353-C0C430011FA6%7D&file=RepoGitHUb.xlsx"
-
-#$Excelfile = "C:\Users\dtodescx\OneDrive - Intel Corporation\RepoGitHUb.xlsx"
-
-	
-	
-	
-	$data = Import-Excel $Excelfile
-	$totalNoOfRecords = $data.count
-	
-	
-	
-	
-	Write-Host $totalNoOfRecords -ForegroundColor White
-	
-	Write-Host $data -ForegroundColor Yellow
-	Write-Host $data.RepositoryName[0] -ForegroundColor Cyan
-	Write-Host $data.Description[0] -ForegroundColor Cyan
-	Write-Host $data.AGS[0] -ForegroundColor Cyan
-	
-	Write-Host $data.RepositoryName[1] -ForegroundColor Magenta
-	Write-Host $data.Description[1] -ForegroundColor Magenta
-	Write-Host $data.AGS[1] -ForegroundColor Magenta
-
-
-
-}
-	
-	Read-Excel
-	Exit
+Get-InstalledModule
