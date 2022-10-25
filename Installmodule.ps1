8788b0d74a63d67f4d82b9efa2bd392329e1ec75
dtodescx <105944588+dtodescx@users.noreply.github.com>
2022-10-25 01:50:53 -0300

    Update Installmodule.ps1

--- Installmodule.ps1
+++ Installmodule.ps1
@@ -11,7 +11,8 @@ Function Read-Excel   {
 
 
 #$Excelfile = "https://intel-my.sharepoint.com/:x:/r/personal/diomendesx_todesco_intel_com/_layouts/15/Doc.aspx?sourcedoc=%7BBE481FE1-E9D8-463C-8353-C0C430011FA6%7D&file=RepoGitHUb.xlsx&action=default&mobileredirect=true&clickparams=eyJBcHBOYW1lIjoiVGVhbXMtRGVza3RvcCIsIkFwcFZlcnNpb24iOiIyNy8yMjA5MDQwMDcxMiIsIkhhc0ZlZGVyYXRlZFVzZXIiOmZhbHNlfQ%3D%3D&cid=c2281e22-e63a-44ac-a46c-b4be14b5ba09"
-$Excelfile = "https://intel-my.sharepoint.com/:x:/r/personal/diomendesx_todesco_intel_com/_layouts/15/Doc.aspx?sourcedoc=%7BBE481FE1-E9D8-463C-8353-C0C430011FA6%7D&file=RepoGitHUb.xlsx"
+#$Excelfile = "https://intel-my.sharepoint.com/:x:/r/personal/diomendesx_todesco_intel_com/_layouts/15/Doc.aspx?sourcedoc=%7BBE481FE1-E9D8-463C-8353-C0C430011FA6%7D&file=RepoGitHUb.xlsx"
+$Excelfile = "https://intel-my.sharepoint.com/:x:/p/diomendesx_todesco/EeEfSL7Y6TxGg1PAxDABH6YBfs5kgCT2nTIhNVUMFSzx3g?email=diomendesx.todesco%40intel.com&e=4%3AH2efNY&at=9&CID=197A1C7C-1894-4386-95FF-D844686A7BD8&wdLOR=cF4D3EF4C-CBF5-4A89-AB4D-FFA1EB858ECE"
 
 #$Excelfile = "C:\Users\dtodescx\OneDrive - Intel Corporation\RepoGitHUb.xlsx"
 
