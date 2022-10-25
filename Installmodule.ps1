1609a23680c2f33a4facaf302890404811507194
dtodescx <105944588+dtodescx@users.noreply.github.com>
2022-10-25 01:29:50 -0300

    Update Installmodule.ps1

--- Installmodule.ps1
+++ Installmodule.ps1
@@ -2,7 +2,7 @@
 Set-PSrepository PSGallery
 
 # Install ImportExcel package
-Install-Module -Name ImportExcel
+Install-Module -Name ImportExcel 3>$null
 
 # List Module installed
 Get-InstalledModule
